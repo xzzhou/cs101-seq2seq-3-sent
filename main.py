f04cb76110ec919a54269a48b51e05fc4534e63b
Xinghao Zhou <xzzhou@caltech.edu>
2018-03-07 11:05:20 -0800

    hidden size to 512 iterations to 77000

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ if use_cuda:
     attn_decoder1 = attn_decoder1.cuda()
     att_decoder3 = attn_decoder3.cuda()
     
-trainIters(encoder1, attn_decoder1, attn_decoder3, input_lang, output_lang, output_lang3, pairs, 105000, print_every=7000)
+trainIters(encoder1, attn_decoder1, attn_decoder3, input_lang, output_lang, output_lang3, pairs, 77000, print_every=7000)
 
 now = str(datetime.datetime.now())[:16].replace('-','_').replace(':', '_').replace(' ', '_')
 save('./savedModel/three_sentences_' + now ,encoder1, attn_decoder1, attn_decoder3)
